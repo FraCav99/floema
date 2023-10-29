d5edd758facb5b1a308b0da46c60bf8220fb9b96
FraCav99 <mastercaster36@outlook.it>
2023-10-29 16:29:56 +0100

    add markup detail page

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const { client } = require('./config/prismicConfig')
 const prismicH = require('@prismicio/helpers')
 
 const app = express()
+const errorHandler = require('errorhandler')
 const port = process.env.PORT || 3000
 
 // Set Pug as templating engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+app.use(errorHandler())
 
 // Add a middleware function that runs on every route. It will inject
 // the prismic context to the locals so that we can access these in
@@ -41,8 +44,11 @@ app.get('/collections', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/detail/:uid', (req, res) => {
-  res.render('pages/detail')
+app.get('/detail/:uid', async (req, res) => {
+  const document = await client.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title',
+  })
+  res.render('pages/detail', { document })
 })
 
 app.listen(port, () => {
